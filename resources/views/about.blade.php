9e18b6067127adae65e3e3c1832e4ad3716e2ea8
Daniel Perez Kacou <kadep198@gmail.com>
2024-04-11 17:08:16 +0000

    the first version of my immo app

---
+++ resources/views/about.blade.php
@@ -0,0 +1,102 @@
+@php
+    Use App\Http\Controllers\Calculator;
+@endphp
+@extends('layouts/base')
+
+@section('title')
+    Immo-A propos
+@endsection
+@section('content')
+
+	
+        <!-- Page Header Start -->
+        <div class="container-fluid page-header mb-5 p-0" style="background-image: url(img/carousel-1.jpg);">
+            <div class="container-fluid page-header-inner py-5">
+                <div class="container text-center pb-5">
+                    <h1 class="display-3 text-white mb-3 animated slideInDown">A Propos de nous</h1>
+                    
+                </div>
+            </div>
+        </div>
+        <!-- Page Header End -->
+
+		
+        <!-- About Start -->
+        <div class="container-xxl py-5">
+            <div class="container">
+                <div class="row g-5 align-items-center">
+                    <div class="col-lg-6">
+                        <h6 class="section-title text-start text-primary text-uppercase">Présentation</h6>
+                        <h1 class="mb-4">Bienvenue à notre <span class="text-primary text-uppercase">Espace Hotelier!</span></h1>
+                        <p class="mb-4">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo magna dolore erat amet</p>
+                        <div class="row g-3 pb-4">
+                            <div class="col-sm-4 wow fadeIn" data-wow-delay="0.1s">
+                                <div class="border rounded p-1">
+                                    <div class="border rounded text-center p-4">
+                                        <i class="fa fa-hotel fa-2x text-primary mb-2"></i>
+                                        <h2 class="mb-1" data-toggle="counter-up">
+                                        @php
+                                            $nombre = (new Calculator())->SommeApparts();
+
+                                        @endphp
+                                        {{$nombre}}
+                                        
+                                        </h2>
+                                        <p class="mb-0">Chambres</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-4 wow fadeIn" data-wow-delay="0.3s">
+                                <div class="border rounded p-1">
+                                    <div class="border rounded text-center p-4">
+                                        <i class="fa fa-users-cog fa-2x text-primary mb-2"></i>
+                                        <h2 class="mb-1" data-toggle="counter-up">
+                                            @php
+                                                $nombre = (new Calculator())->SommeUsers();
+
+                                            @endphp
+                                            {{$nombre}}
+                                        </h2>
+                                        <p class="mb-0">Employés</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-4 wow fadeIn" data-wow-delay="0.5s">
+                                <div class="border rounded p-1">
+                                    <div class="border rounded text-center p-4">
+                                        <i class="fa fa-users fa-2x text-primary mb-2"></i>
+                                        <h2 class="mb-1" data-toggle="counter-up">
+                                            @php
+                                                $nombre = (new Calculator())->SommeCustomers();
+
+                                            @endphp
+                                            {{$nombre}}
+                                        </h2>
+                                        <p class="mb-0">Clients</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="row g-3">
+                            <div class="col-6 text-end">
+                                <img class="img-fluid rounded w-75 wow zoomIn" data-wow-delay="0.1s" src="img/about-1.jpg" style="margin-top: 25%;">
+                            </div>
+                            <div class="col-6 text-start">
+                                <img class="img-fluid rounded w-100 wow zoomIn" data-wow-delay="0.3s" src="img/about-2.jpg">
+                            </div>
+                            <div class="col-6 text-end">
+                                <img class="img-fluid rounded w-50 wow zoomIn" data-wow-delay="0.5s" src="img/about-3.jpg">
+                            </div>
+                            <div class="col-6 text-start">
+                                <img class="img-fluid rounded w-75 wow zoomIn" data-wow-delay="0.7s" src="img/about-4.jpg">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- About End -->
+@endsection
